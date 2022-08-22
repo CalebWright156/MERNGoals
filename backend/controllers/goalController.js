a7a49fa415326f8b130d60451254f9b215566cb4
Caleb Wright <calebwright156@gmail.com>
2022-08-22 20:56:42 +1200

    finalised basic crud on backend

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,11 +1,17 @@
 const asyncHandler = require('express-async-handler')
 
+const Goal = require('../models/goalModel')
+
 
 const { restart } = require("nodemon")
 
+// Desc: get goals
+// route: GET api/goals
 // Access: Private
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({message: "get goals"})
+    const goals = await Goal.find()
+
+    res.status(200).json(goals)
 }) 
 
 // Desc: Set goals
@@ -17,21 +23,40 @@ const setGoal = asyncHandler(async (req, res) => {
         throw new Error('Please add a text field')
     }
 
-    res.status(200).json({message: "Set goal"})
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+
+    res.status(200).json(goal)
 }) 
 
 // Desc: Update goals
 // route: PUT/api/goals/:id
 // Access: Private
 const updateGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Update goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
+
+    res.status(200).json(updatedGoal)
 }) 
 
 // Desc: Delete goals
 // route: DELETE/api/goals/:id
 // Access: Private
 const deleteGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Delete goal ${req.params.id}`})
+    const goal = await Goal.findByIdAndDelete(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    res.status(200).json({ id: req.params.id })
 }) 
 
 module.exports = {
